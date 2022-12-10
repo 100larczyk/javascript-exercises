17d0fe1f1469ebb3716b961b7e772c3d0047de3e
100larczyk <100larczyk.m@gmail.com>
2022-12-10 22:11:35 +0100

    first changes to 5th exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+  let firstValue = Number(a);
+  let secondValue = Number(b);
+  let i = firstValue;
+  let finalSum;
+  for (i; i <= secondValue; i++) {
+    finalSum += i;
+  }
+  return finalSum;
 };
 
+sumAll(2, 6);
 // Do not edit below this line
 module.exports = sumAll;
